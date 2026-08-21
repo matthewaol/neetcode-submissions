45bbc3ea3d09a2d80358682a91637182379633f1
Matthew Dacanay <51929639+matthewaol@users.noreply.github.com>
2026-08-20 20:32:18 -0700

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists) == 0: return None
+
+        res = ListNode(0)
+        cur = res
+        minHeap = []
+
+        for l in lists:
+            while l:
+                heapq.heappush(minHeap, l.val)
+                l = l.next
+        
+        if not minHeap:
+            return None
+
+        root = ListNode(heapq.heappop(minHeap))
+        curr = root
+        while minHeap:
+            curr.next = ListNode(heapq.heappop(minHeap))
+            curr = curr.next
+
+        return root
